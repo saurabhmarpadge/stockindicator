73818aa461f63c33ed1cc9c19976f62b629e2e5e
saurabhmarpadge <saurabhmarpadge29@gmail.com>
2017-03-16 18:33:54 +0530

    Fixes Bootstrap version problem

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -19,7 +19,7 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="<?php echo BASE_URI;?>templates/js/jquery.js"></script>
+<script src="<?php echo BASE_URI;?>templates/js/jquery-2.0.0.js"></script>
 <!--  <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>-->
 <script src="<?php echo BASE_URI;?>templates/js/bootstrap.js"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
